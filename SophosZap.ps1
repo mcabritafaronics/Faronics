e4a052bc1eeeeeaecd361a9eb798b4be19aba869
mcabritafaronics <104034655+mcabritafaronics@users.noreply.github.com>
2022-08-16 13:37:50 -0700

    Add files via upload

---
+++ SophosZap.ps1
@@ -0,0 +1,15 @@
+﻿# Sophoszap 
+$SophosZAPdir="C:\temp\SophosRemoval"
+$SophosZAP= "C:\Temp\SophosRemoval\SophosZap.exe"
+$args= "--confirm"
+
+#Enter your Network location for source file
+$SourceZAP = "\\tsclient\C\SOURCE\Sophos\SophosZap.exe"
+
+
+mkdir $SophosZAPdir
+Copy-Item $SourceZAP $SophosZAPdir
+
+#Start-Process  -FilePath $SophosZAP -ArgumentList "--confirm" 
+#Invoke-Expression $sophoszap $args
+& $SophosZAP $args
