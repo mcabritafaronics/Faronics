003c85030e7d3687c06faff647471d6f7716f1f2
mcabritafaronics <104034655+mcabritafaronics@users.noreply.github.com>
2023-03-20 15:04:11 -0700

    Add files via upload

---
+++ WInselect.ps1
@@ -0,0 +1,12 @@
+﻿#WinSelect Powershell File
+#Created March 20, 2023
+#By Michael Cabrita
+
+#Rename Winselect
+Rename-Item -Path "C:\Program Files\Faronics\WINSelect\WSEngine.exe" -NewName "WSEngineOLD.OLD"
+
+#Copy WInselect files from github to computer
+Invoke-WebRequest -Uri https://github.com/mcabritafaronics/Faronics/raw/main/WSEngine.exe -OutFile "C:\Program Files\Faronics\WINSelect\WSEngine.exe"
+
+#Restart Computer
+Restart-Computer -Force
