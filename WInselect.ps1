fe78f4f951b5ebf999ffc16a4417c9563a05f13f
mcabritafaronics <104034655+mcabritafaronics@users.noreply.github.com>
2023-03-20 15:57:46 -0700

    Update WInselect.ps1

--- WInselect.ps1
+++ WInselect.ps1
@@ -8,5 +8,8 @@ Rename-Item -Path "C:\Program Files\Faronics\WINSelect\WSEngine.exe" -NewName "W
 #Copy WInselect files from github to computer
 Invoke-WebRequest -Uri https://github.com/mcabritafaronics/Faronics/raw/main/WSEngine.exe -OutFile "C:\Program Files\Faronics\WINSelect\WSEngine.exe"
 
+#Copy Launch Application
+invoke-webrequest -Uri https://github.com/mcabritafaronics/Faronics/raw/main/LaunchModernApp.exe -Outfile "C:\Program Files\Faronics\WINSelect\LaunchModernApp.exe"
+
 #Restart Computer
 Restart-Computer -Force
