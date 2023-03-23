d5e616a7dee5f19117c2507953540d74c4143394
mcabritafaronics <104034655+mcabritafaronics@users.noreply.github.com>
2023-03-23 12:11:33 -0700

    Update Remove-PreviousOfficeInstalls.ps1

--- Remove-PreviousOfficeInstalls.ps1
+++ Remove-PreviousOfficeInstalls.ps1
@@ -5,5 +5,4 @@
 mkdir "c:\temp_officeremoval\"
 invoke-webrequest  https://github.com/mcabritafaronics/Faronics/raw/main/SaRACmd_17_0_9663_9.zip -outfile c:\temp_officeremoval\SaRACmd_17_0_9663_9.zip
 Expand-Archive c:\temp_officeremoval\SaRACmd_17_0_9663_9.zip -DestinationPath c:\temp_officeremoval\
-Start-Sleep -Seconds 10
 Start-Process C:\temp_officeremoval\SaRAcmd.exe -ArgumentList "-S OfficeScrubScenario -AcceptEula -OfficeVersion All"
