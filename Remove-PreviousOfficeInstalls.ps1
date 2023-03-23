c2ffd3df32e2fb5a7ff67f1616896d6482544821
mcabritafaronics <104034655+mcabritafaronics@users.noreply.github.com>
2023-03-23 11:07:20 -0700

    Add files via upload

---
+++ Remove-PreviousOfficeInstalls.ps1
@@ -0,0 +1,8 @@
+﻿#Uninstall Office Applications
+
+mkdir c:\temp_officeremoval\
+mkdir c:\temp_officeremoval\zip\
+
+invoke-webrequest https://aka.ms/SaRA_EnterpriseVersionFiles -outfile c:\temp_officeremoval\zip\
+Expand-Archive c:\temp_officeremoval\zip\SaRACmd_17_0_9663_9.zip -DestinationPath c:\temp_officeremoval\
+Start-Process "C:\temp_officeremoval\SaRAcmd.exe -S OfficeScrubScenario -AcceptEula -OfficeVersion All"
