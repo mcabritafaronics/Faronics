50bafd75285ba04f0c0de5c82c7efa607b9f96a3
mcabritafaronics <104034655+mcabritafaronics@users.noreply.github.com>
2023-03-23 11:19:39 -0700

    Update Remove-PreviousOfficeInstalls.ps1

--- Remove-PreviousOfficeInstalls.ps1
+++ Remove-PreviousOfficeInstalls.ps1
@@ -1,8 +1,6 @@
-﻿#Uninstall Office Applications
+#Uninstall Office Applications
 
 mkdir c:\temp_officeremoval\
-mkdir c:\temp_officeremoval\zip\
-
-invoke-webrequest https://aka.ms/SaRA_EnterpriseVersionFiles -outfile c:\temp_officeremoval\zip\
-Expand-Archive c:\temp_officeremoval\zip\SaRACmd_17_0_9663_9.zip -DestinationPath c:\temp_officeremoval\
+invoke-webrequest  https://github.com/mcabritafaronics/Faronics/raw/main/SaRACmd_17_0_9663_9.zip -outfile c:\temp_officeremoval\SaRACmd_17_0_9663_9.zip
+Expand-Archive c:\temp_officeremoval\SaRACmd_17_0_9663_9.zip -DestinationPath c:\temp_officeremoval\
 Start-Process "C:\temp_officeremoval\SaRAcmd.exe -S OfficeScrubScenario -AcceptEula -OfficeVersion All"
