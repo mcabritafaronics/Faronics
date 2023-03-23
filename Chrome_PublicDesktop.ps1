847efce2047e2177927a4a010366ad2ae97ab5ad
mcabritafaronics <104034655+mcabritafaronics@users.noreply.github.com>
2023-03-23 12:42:58 -0700

    Add files via upload

---
+++ Chrome_PublicDesktop.ps1
@@ -0,0 +1,7 @@
+﻿$Shell = New-Object -ComObject (“WScript.Shell”)
+#$destination = $new_object.SpecialFolders.Item("AllUsersDesktop")
+#$ShortCut = $Shell.CreateShortcut("Desktop\Office.lnk”)
+$ShortCut = $Shell.CreateShortcut("$env:Public\Desktop\Office.lnk”)
+$ShortCut.TargetPath="C:\Program Files\Google\Chrome\Application\chrome.exe"
+$ShortCut.Arguments=“https://office.com”
+$ShortCut.Save()
